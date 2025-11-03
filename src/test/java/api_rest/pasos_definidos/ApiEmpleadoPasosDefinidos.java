db2d626c6f48aebb7c3b1eb7cda1d3daff55ec47
Yesid <faybersanabria@gmail.com>
2025-11-02 23:41:36 -0500

    Ajustar yml CI/CD

--- src/test/java/api_rest/pasos_definidos/ApiEmpleadoPasosDefinidos.java
+++ src/test/java/api_rest/pasos_definidos/ApiEmpleadoPasosDefinidos.java
@@ -14,7 +14,7 @@ import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 public class ApiEmpleadoPasosDefinidos {
   private Actor actor = Actor.named("Fayber");
   private final String BASE_URL = "http://localhost:80";
-  private final String COOKIE = "p4rudg362gcn30vgaur8n2bnc8";
+  private final String COOKIE = "j5qolekquna97jjs99hvt3f5u0";
 
   @Given("que el usuario tiene acceso a la API de OrangeHRM")
   public void queElUsuarioTieneAccesoALaAPI() {
